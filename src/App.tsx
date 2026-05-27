996438492eca17b16cfe49541b209d184f808fe4
David Snir <david.snir@gmail.com>
2026-05-27 13:58:10 +0300

    changed title

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
 
   return (
     <div>
-      <h1>Harmonic Haven</h1>
+      <h1>Eli</h1>
       {tracks.length === 0 ? (
         <p>No tracks in playlist</p>
       ) : (
